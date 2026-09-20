a028a285315651e9b5f0aba5a030eb2d280f03de
wixie <phantanlong645@gmail.com>
2026-09-20 16:41:26 +0800

    回文链表复习

--- problems/0234/go/main.go
+++ problems/0234/go/main.go
@@ -1,57 +1,76 @@
 package main
 
+import (
+	"fmt"
+)
+
 type ListNode struct {
-	Val  int
-	Next *ListNode
+	value int
+	Next  *ListNode
 }
 
 func main() {
+	l1 := &ListNode{
+		value: 1,
+		Next:  nil,
+	}
+	l2 := &ListNode{
+		value: 2,
+		Next:  nil,
+	}
+	l3 := &ListNode{
+		value: 3,
+		Next:  nil,
+	}
+	l4 := &ListNode{
+		value: 1,
+		Next:  nil,
+	}
+	l1.Next = l2
+	l2.Next = l3
+	l3.Next = l4
+	res := isPalindrome(l1)
+	fmt.Println(res)
 
 }
 func isPalindrome(head *ListNode) bool {
+	var cnt int
+	cnt = 0
 	l1 := head
-	cnt := 0
-
 	for l1 != nil {
 		cnt++
 		l1 = l1.Next
-
 	}
-	if cnt == 1 {
-		return true
-	}
-	s1 := make([]int, cnt)
 	l1 = head
+	var m []*ListNode
 	if cnt%2 == 0 {
-		for i := 0; i < cnt/2; i++ {
-			s1[i] = l1.Val
+		for i := 1; i <= cnt/2; i++ {
+			m = append(m, l1)
 			l1 = l1.Next
 		}
-		k := cnt/2 - 1
+		j := 0
 		for l1 != nil {
-
-			if l1.Val != s1[k] {
+			if l1.value != m[cnt/2-j-1].value {
 				return false
 			}
-			k--
 			l1 = l1.Next
+			j++
 		}
-	}
-	if cnt%2 == 1 {
-		for i := 0; i < cnt/2; i++ {
-			s1[i] = l1.Val
+	} else {
+		for i := 1; i <= cnt/2; i++ {
+			m = append(m, l1)
 			l1 = l1.Next
 		}
 		l1 = l1.Next
-		k := cnt/2 - 1
+		j := 0
 		for l1 != nil {
-
-			if l1.Val != s1[k] {
+			if l1.value != m[cnt/2-j-1].value {
 				return false
 			}
-			k--
 			l1 = l1.Next
+			j++
 		}
 	}
 	return true
+
 }
