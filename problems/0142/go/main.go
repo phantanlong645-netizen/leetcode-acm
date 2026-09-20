700188dc4013f9e2a07a3600a406f8b480a1bc7d
wixie <phantanlong645@gmail.com>
2026-09-20 17:31:53 +0800

    环形链表2复习

--- problems/0142/go/main.go
+++ problems/0142/go/main.go
@@ -1,25 +1,48 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func main() {
+	l1 := &ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	l2 := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	l3 := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	l4 := &ListNode{
+		Val:  -4,
+		Next: nil,
+	}
+	l1.Next = l2
+	l2.Next = l3
+	l3.Next = l4
+	l4.Next = l2
+	res := detectCycle(l1)
+	fmt.Println(res.Val)
 
 }
 func detectCycle(head *ListNode) *ListNode {
+	l1 := head
 	m := map[*ListNode]int{}
-	l := head
-	cnt := 0
-	for l != nil {
-		if _, ok := m[l]; ok {
-			return l
+	for l1 != nil {
+		if _, ok := m[l1]; ok {
+			return l1
 		}
-		m[l] = cnt
-		cnt++
-		l = l.Next
-	}
 
+		m[l1] = 1
+		l1 = l1.Next
+	}
 	return nil
+
 }
