8879bd3970e86d5ee59b70256890abdab46c3734
wixie <phantanlong645@gmail.com>
2026-09-20 22:34:01 +0800

    两数相加    合并两个有序链表

--- problems/0002/go/main.go
+++ problems/0002/go/main.go
@@ -1,43 +1,98 @@
 package main
 
+import (
+	"fmt"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func main() {
+	a1 := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	a2 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	a3 := &ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	a1.Next = a2
+	a2.Next = a3
+	b1 := &ListNode{
+		Val:  5,
+		Next: nil,
+	}
+	b2 := &ListNode{
+		Val:  6,
+		Next: nil,
+	}
+	b3 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	b1.Next = b2
+	b2.Next = b3
+	c := addTwoNumbers(a1, b1)
+	for c != nil {
+		fmt.Println(c.Val)
+		c = c.Next
+	}
 
 }
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	pre := &ListNode{}
-	res := pre
-	n := 0
-	sum := 0
-	n1 := 0
-	n2 := 0
+	var n1 int
+	var n int
+	l3 := &ListNode{}
+	head := l3
+	n = 0
 	for l1 != nil || l2 != nil {
-		if l1 != nil {
-			n1 = l1.Val
-			l1 = l1.Next
-		} else {
-			n1 = 0
+		sum := l1.Val + l2.Val + n
+		n1 = sum % 10
+		n = sum / 10
+		tmp := &ListNode{
+			Val:  n1,
+			Next: nil,
 		}
-		if l2 != nil {
-			n2 = l2.Val
-			l2 = l2.Next
-		} else {
-			n2 = 0
+		l3.Next = tmp
+		l3 = l3.Next
+		l1 = l1.Next
+		l2 = l2.Next
+	}
+	if l1 != nil {
+		k1 := l1
+		for k1 != nil {
+			sum := k1.Val + n
+			n = sum / 10
+			n2 := sum % 10
+			tmp := &ListNode{
+				Val:  n2,
+				Next: nil,
+			}
+			l3.Next = tmp
+			l3 = l3.Next
+			k1 = k1.Next
 		}
-		sum = (n1 + n2 + n) % 10
-		n = (n1 + n2 + n) / 10
-		cur := &ListNode{Val: sum}
-		pre.Next = cur
-		pre = cur
 	}
-	if n > 0 {
-		cur := &ListNode{Val: n}
-		pre.Next = cur
-
+	if l2 != nil {
+		k1 := l1
+		for k1 != nil {
+			sum := k1.Val + n
+			n = sum / 10
+			n2 := sum % 10
+			tmp := &ListNode{
+				Val:  n2,
+				Next: nil,
+			}
+			l3.Next = tmp
+			l3 = l3.Next
+			k1 = k1.Next
+		}
 	}
-	return res.Next
+	return head.Next
 }
--- problems/0021/go/main.go
+++ problems/0021/go/main.go
@@ -1,36 +1,85 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func main() {
+	a1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	a2 := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	a3 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	a1.Next = a2
+	a2.Next = a3
+	b1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	b2 := &ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	b3 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	b1.Next = b2
+	b2.Next = b3
+	l := mergeTwoLists(a1, b1)
+	for l != nil {
+		fmt.Println(l.Val)
+		l = l.Next
+	}
 
 }
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	l1 := list1
 	l2 := list2
-	l3 := &ListNode{}
-	head := l3
-	for l1 != nil && l2 != nil {
-		if l1.Val <= l2.Val {
-			l3.Next = l1
-			l3 = l1
-			l1 = l1.Next
-		} else {
-			l3.Next = l2
-			l3 = l2
-			l2 = l2.Next
-		}
-
+	var l3 *ListNode
+	if l1 == nil && l2 == nil {
+		return nil
 	}
 	if l1 == nil {
-		l3.Next = l2
+		return l2
 	}
 	if l2 == nil {
-		l3.Next = l1
+		return l1
 	}
-	return head.Next
-
+	if l1.Val < l2.Val {
+		l3 = l1
+		l1 = l1.Next
+	} else {
+		l3 = l2
+		l2 = l2.Next
+	}
+	tmp := l3
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			tmp.Next = l1
+			tmp = tmp.Next
+			l1 = l1.Next
+		} else {
+			tmp.Next = l2
+			tmp = tmp.Next
+			l2 = l2.Next
+		}
+	}
+	if l1 != nil {
+		tmp.Next = l1
+	}
+	if l2 != nil {
+		tmp.Next = l2
+	}
+	return l3
 }
