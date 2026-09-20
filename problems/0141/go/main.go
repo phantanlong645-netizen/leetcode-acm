3818a995d55bdf88325362a63f7e5afc4c70ec50
wixie <phantanlong645@gmail.com>
2026-09-20 17:20:10 +0800

    环形链表复习

--- problems/0141/go/main.go
+++ problems/0141/go/main.go
@@ -1,30 +1,47 @@
 package main
 
-func main() {
+import "fmt"
 
+type ListNode struct {
+	val  int
+	Next *ListNode
 }
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-type ListNode struct {
-	Val  int
-	Next *ListNode
+func main() {
+	l1 := &ListNode{
+		val:  3,
+		Next: nil,
+	}
+	l2 := &ListNode{
+		val:  2,
+		Next: nil,
+	}
+	l3 := &ListNode{
+		val:  0,
+		Next: nil,
+	}
+	l4 := &ListNode{
+		val:  -4,
+		Next: nil,
+	}
+	l1.Next = l2
+	l2.Next = l3
+	l3.Next = l4
+	l4.Next = l2
+	res := hasCycle(l1)
+	fmt.Println(res)
 }
 
 func hasCycle(head *ListNode) bool {
 	m := map[*ListNode]bool{}
-	h := head
-	for h != nil {
-		if m[h] {
+	l1 := head
+	for l1 != nil {
+		if _, ok := m[l1]; ok {
 			return true
 		}
-		m[h] = true
-		h = h.Next
+		m[l1] = true
+		l1 = l1.Next
 	}
 	return false
+
 }
