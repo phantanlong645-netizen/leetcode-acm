bd83cf17349742a0a8bde7d75f091db14311908a
wixie <phantanlong645@gmail.com>
2026-09-21 12:59:23 +0800

    两两交换链表节点

--- problems/0024/go/mian.go
+++ problems/0024/go/mian.go
@@ -1,11 +1,37 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func main() {
+	n1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	n2 := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	n3 := &ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	n4 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	n1.Next = n2
+	n2.Next = n3
+	n3.Next = n4
+	L := swapPairs(n1)
+	for L != nil {
+		fmt.Println(L.Val)
+		L = L.Next
+	}
 
 }
 
@@ -25,23 +51,22 @@ func swapPairs(head *ListNode) *ListNode {
 	}
 	l1 := head
 	l2 := head.Next
-	k := l2
-	temp := l2.Next
+	K := l2
 	l3 := &ListNode{}
-	l3.Next = head
+	l3.Next = l1
 	for l1 != nil && l2 != nil {
+		tmp := l2.Next
 		l2.Next = l1
-		l1.Next = temp
+		l1.Next = tmp
 		l3.Next = l2
 		l3 = l1
-		l1 = temp
+		l1 = tmp
 		if l1 != nil && l1.Next != nil {
 			l2 = l1.Next
-			temp = l2.Next
 		} else {
 			break
 		}
-	}
-	return k
 
+	}
+	return K
 }
