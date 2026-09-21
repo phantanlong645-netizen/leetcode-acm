e3032a1dd235b1a20f29802a546bc5a9b1e112b8
wixie <phantanlong645@gmail.com>
2026-09-21 10:41:18 +0800

    删除倒数第N个链表节点

--- problems/0019/go/main‘.go
+++ problems/0019/go/main‘.go
@@ -1,32 +1,61 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func main() {
-
+	l1 := &ListNode{
+		Val:  1,
+		Next: nil,
+	}
+	l2 := &ListNode{
+		Val:  2,
+		Next: nil,
+	}
+	l3 := &ListNode{
+		Val:  3,
+		Next: nil,
+	}
+	l4 := &ListNode{
+		Val:  4,
+		Next: nil,
+	}
+	l5 := &ListNode{
+		Val:  5,
+		Next: nil,
+	}
+	l1.Next = l2
+	l2.Next = l3
+	l3.Next = l4
+	l4.Next = l5
+	n := 2
+	l := removeNthFromEnd(l1, n)
+	for l != nil {
+		fmt.Println(l.Val)
+		l = l.Next
+	}
 }
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	l1 := head
-	ans := 0
-	for l1 != nil {
-		ans++
-		l1 = l1.Next
+	l := head
+	var cnt int
+	cnt = 0
+	for l != nil {
+		cnt++
+		l = l.Next
 	}
-	if ans == 1 {
-		return nil
-	}
-	l1 = head
-	k := ans - n
-	if ans == n {
+	l = head
+	if cnt == n {
 		return head.Next
 	}
-	for i := 0; i < k-1; i++ {
-		l1 = l1.Next
+	for i := 0; i < cnt-n-1; i++ {
+		l = l.Next
 	}
-	l2 := l1.Next
-	l1.Next = l2.Next
+	tmp := l.Next
+	l.Next = tmp.Next
 	return head
+
 }
